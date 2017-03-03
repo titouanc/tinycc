d9cf1e952dc6e178762797e1dcf544d8723a06fe
iTitou <moiandme@gmail.com>
2017-03-03 21:38:24 +0100

    [enh] Better main function (reading from stdin)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ pub mod lexer;
 pub mod ast;
 pub mod parser;
 
+use std::io::{self, Read};
+
 fn main() {
-    parser::parse_string("char lol; int f(int x, int y){return x + y;}", true);
+    let mut buffer = String::new();
+    if let Ok(_) = io::stdin().read_to_string(&mut buffer) {
+        let tokens = lexer::Lexer::new(buffer.as_str());
+        for (t, _) in tokens {
+            print!("{:?} ", t);
+        }
+        println!("");
+        parser::parse_string(buffer.as_str(), true);
+    }    
 }

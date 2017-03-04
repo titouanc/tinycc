207de7164545d63347bad7ec0e598f801dec756c
iTitou <moiandme@gmail.com>
2017-03-04 01:35:16 +0100

    [fix] Filename + no need for tokens in main anymore

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ pub mod tokens;
 pub mod lexer;
 pub mod ast;
 pub mod parser;
+pub mod staticanalysis;
 
 use std::io::{self, Read};
 
 fn main() {
     let mut buffer = String::new();
     if let Ok(_) = io::stdin().read_to_string(&mut buffer) {
-        let tokens = lexer::Lexer::new(buffer.as_str());
-        for (t, _) in tokens {
-            print!("{:?} ", t);
-        }
-        println!("");
-        parser::parse_string(buffer.as_str(), true);
+        let res = parser::parse_string(buffer.as_str(), true);
+        println!("{:?}", res);
     }    
 }
